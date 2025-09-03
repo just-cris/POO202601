f29149f25b198349c9f6c48bc5f94e12dcfa77cc
just-cris <cristian.cardenas2905@gmail.com>
2025-09-03 11:39:28 -0600

    Add Character class and usage example in main.cpp
    Introduces a Character class with health management and rebirth functionality. Updates main.cpp to demonstrate creating a Character, modifying its health, and invoking rebirth logic. Also updates project file paths for source and header files.

--- POO202601/source/main.cpp
+++ POO202601/source/main.cpp
@@ -1,6 +1,59 @@
+#pragma once
+
+//STD
+#include <iostream>
+#include <string>
+#include <vector>
+
+//Crear clase Character
+class
+	Character {
+public:
+	Character() = default;
+
+	Character(int healt) : m_healt(healt) {}
+
+	void
+		rebirth() {
+		if (!isCharacterAlive && m_health <= 0) {
+			std::cout << "El personaje a revivido" << std::endl;
+			isCharacterAlive = true;
+		}
+	}
+
+	int
+		getHealt() const {
+		return m_healt;
+	}
+
+	void
+		setHealt(int healt) {
+		m_healt = healt;
+	}
+
+private:
+	bool isCharacterAlive = false;
+
+protected:
+	int m_healt;
+};
+
+
+//Crear clase Inventario
+
 
 
 int
 main() {
+	Character pablo(150);
+	pablo.getHealt();
+	std::cout << pablo.getHealt() << std::endl;
+	pablo.setHealt(-50);
+	std::cout << pablo.getHealt() << std::endl;
+
+	if (pablo.getHealt() <= 0) {
+		std::cout << "Pablo ha muerto" << std::endl;
+		pablo.rebirth();
+	}
 	return 0;
 }
